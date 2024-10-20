2176ff8da0193370597550c7536ee9490c93f745
davidDenis17 <davidalejandro1.dadg@gmail.com>
2024-10-20 16:48:31 -0400

    improved foreach syntax

--- Step Tracker/HealthDataListView.swift
+++ Step Tracker/HealthDataListView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct HealthDataListView: View {
-    
+
     @State private var isShowingAddData = false
     @State private var addDataDate: Date = .now
     @State private var valueToAdd: String = ""
     var metric: HealthMetricContext
-    
-    
+
     var body: some View {
         List(0..<28) { i in
             HStack {
                 Text(Date(), format: .dateTime.month().day().year())
                 Spacer()
-                Text(10000, format: .number.precision(.fractionLength(metric == .steps ? 0 : 1)))
+                Text(
+                    10000,
+                    format: .number.precision(
+                        .fractionLength(metric == .steps ? 0 : 1)))
             }
         }
         .navigationTitle(metric.title)
@@ -36,23 +38,28 @@ struct HealthDataListView: View {
             }
         }
     }
-    
-    
+
     var addDataView: some View {
         NavigationStack {
             Form {
-                DatePicker("Add Data Date", selection: $addDataDate, displayedComponents: .date)
+                DatePicker(
+                    "Add Data Date", selection: $addDataDate,
+                    displayedComponents: .date)
                 HStack {
                     Text(metric.title)
                     Spacer()
                     TextField("Value", text: $valueToAdd)
                         .multilineTextAlignment(.trailing)
                         .frame(width: 140)
-                        .keyboardType(metric == .steps ? .numberPad : .decimalPad)
+                        .keyboardType(
+                            metric == .steps ? .numberPad : .decimalPad)
                 }
-                
+
             }
-            .background(Color.secondary.opacity(0.1).clipShape(RoundedRectangle(cornerRadius: 30)))
+            .background(
+                Color.secondary.opacity(0.1).clipShape(
+                    RoundedRectangle(cornerRadius: 30))
+            )
             .scrollContentBackground(.hidden)
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
@@ -60,7 +67,7 @@ struct HealthDataListView: View {
                         // Code Later
                     }
                 }
-                
+
                 ToolbarItem(placement: .topBarLeading) {
                     Button("Dismiss") {
                         isShowingAddData = false
@@ -68,9 +75,9 @@ struct HealthDataListView: View {
                 }
             }
         }
-        
+
     }
-    
+
 }
 
 #Preview {
